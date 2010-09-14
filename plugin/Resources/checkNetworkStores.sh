7735c636eff6ebcb0ff1235cc4340ec9bc2ebf5d
Antonin Hildebrand <antonin@hildebrand.cz>
2010-09-14 17:42:40 +0200

    fixing buggy 'Dont write .DS_Store to Network' checkbox

--- plugin/Resources/checkNetworkStores.sh
+++ plugin/Resources/checkNetworkStores.sh
@@ -1,1 +1,6 @@
-defaults read com.apple.desktopservices DSDontWriteNetworkStores
+#!/bin/sh
+STATUS=`defaults read com.apple.desktopservices DSDontWriteNetworkStores`
+if [ "$STATUS" == "true" ]; then
+    exit 1
+fi
+exit 0
--- plugin/Resources/enableNetworkStores.sh
+++ plugin/Resources/enableNetworkStores.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 defaults write com.apple.desktopservices DSDontWriteNetworkStores false
