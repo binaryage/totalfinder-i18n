43d367eb58f39e7c3032e51a237eb5399b0f5722
Carlos Precioso <kalprestito@yahoo.es>
2010-12-16 14:16:25 +0100

    Improved sync.sh: now it asks to add remote git and to push to GitHub

--- sync.sh
+++ sync.sh
@@ -3,9 +3,21 @@
 echo "This may end in conflict, see git docs."
 
 #point to server
-git remote add ba git://github.com/binaryage/totalfinder-i18n.git
+read -p "Is this the first time you sync? [y/n] " -n 1
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	git remote add ba git://github.com/binaryage/totalfinder-i18n.git
+	echo "Pointed!"
+fi
 
 #merge repos
 git fetch ba && git merge ba/master
+echo "Merged!"
 
-echo "Synced!"
+#push to GitHub
+read -p "Do you want to push to GitHub? This will update the online repo with any commit you made since last push. Until you do this, changes won't be in your GitHub fork. [y/n] " -n 1
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	git push
+	echo "Pushed!"
+fi
