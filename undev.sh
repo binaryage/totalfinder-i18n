6b666e6d5e82b7d6f671e8bf5a8964b35ca29031
Antonin Hildebrand <antonin@hildebrand.cz>
2011-09-16 11:16:26 +0200

    update TotalFinder.bundle paths to point to a new location (since 1.2.5)

--- undev.sh
+++ undev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TOTALFINDER_RESOURCES='/Applications/TotalFinder.app/Contents/Resources/TotalFinder.bundle/Contents/Resources'
-TOTALFINDER_RESOURCES_BACKUP='/Applications/TotalFinder.app/Contents/Resources/TotalFinder.bundle/Contents/ResourcesOrig'
+TOTALFINDER_RESOURCES='/Library/ScriptingAdditions/TotalFinder.osax/Contents/Resources/TotalFinder.bundle/Contents/Resources'
+TOTALFINDER_RESOURCES_BACKUP='/Library/ScriptingAdditions/TotalFinder.osax/Contents/Resources/TotalFinder.bundle/Contents/ResourcesOrig'
 
 if [ -d "$TOTALFINDER_RESOURCES" ]; then # is it a folder?
     if [ -L "$TOTALFINDER_RESOURCES" ]; then # is is a symlink?
