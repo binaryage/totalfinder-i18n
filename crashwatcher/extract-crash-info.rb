885a6a559c1f4a3f8d20499ec31642d0c6d2505c
Antonin Hildebrand <antonin@hildebrand.cz>
2013-07-09 13:42:06 +0200

    try to be smarter in extracting crashed plugin name...
    use callstack if Plugin Identifier is not available

--- crashwatcher/extract-crash-info.rb
+++ crashwatcher/extract-crash-info.rb
@@ -1,44 +1,79 @@
 #!/usr/bin/env ruby
 
+def parse_plugin_identifier(content)
+  m = content.match(/PlugIn Identifier:(.*?)\n/m)
+  return nil if m.nil?
+  m[1]
+end
+
+def find_first_ba_module(content)
+  addresses = []
+  ranges = []
+
+  content.lines.each do |line|
+    # extract adressess from callstacks on all threads
+    # 8   com.apple.AppKit        	0x00007fff88a0d68f -[NSApplication run] + 395
+    if line =~ /^\s*\d+\s+.*?0x([0-9a-f]+)\s/ then
+      addresses << $1.to_i(16)
+    end
+
+    # extract loaded module ranges, filter out only com.binaryage related
+    # 0x1181c2000 -    0x1181c3ff7 +com.binaryage.totalfinder.nodesktopdots ##VERSION## (##VERSION##) <1243063C-4405-3DD6-8AC8-816EFA979801> /Applications/TotalFinder.app/Contents/Resources/TotalFinder.bundle/Contents/PlugIns/NoDesktopDots.bundle/Contents/MacOS/NoDesktopDots
+    if line =~ /^\s*0x([0-9a-f]+)\s+-\s+0x([0-9a-f]+)\s+.*?(com\.binaryage.*?)\s/ then
+      ranges << [$1.to_i(16), $2.to_i(16), $3]
+    end
+  end
+
+  # test if any address is present inside our ranges
+  addresses.each do |address|
+    ranges.each do |range|
+      hit = address >= range[0] && address <= range[1]
+      return range[2] if hit
+    end
+  end
+end
+
 file = ARGV[0]
 content = File.read(file)
 
 res = ""
 
 begin
-    plugin = content.match(/PlugIn Identifier:(.*?)\n/m)[1]
-    res += "in " + plugin.strip + " "
+  plugin = parse_plugin_identifier(content)
+  plugin = find_first_ba_module(content) unless plugin
+  plugin.strip!
+  res += "in " + plugin.gsub("com.binaryage.", "") + " " if plugin.size
 rescue
 end
 
 details = []
 
 begin
-    version = content.match(/Version:(.*?)\n/m)[1].split(" ")[0]
-    details << "v"+version.strip
+  version = content.match(/Version:(.*?)\n/m)[1].split(" ")[0]
+  details << "v"+version.strip
 rescue
 end
 
 begin
-    thread = content.match(/Crashed Thread:(.*?)\n/m)[1].split(" ")[0]
-    details << "thread "+thread.strip
+  thread = content.match(/Crashed Thread:(.*?)\n/m)[1].split(" ")[0]
+  details << "thread "+thread.strip
 rescue
 end
 
 begin
-    type = content.match(/Exception Type:(.*?)\n/m)[1]
-    x = type.match(/(.*?)\(.*\)/)
-    type = x[1] if x # remove braced part if present
-    type.downcase! 
-    type.sub!("exc_", "")
-    type.sub!("_", " ")
-    details << type.strip
+  type = content.match(/Exception Type:(.*?)\n/m)[1]
+  x = type.match(/(.*?)\(.*\)/)
+  type = x[1] if x # remove braced part if present
+  type.downcase!
+  type.sub!("exc_", "")
+  type.sub!("_", " ")
+  details << type.strip
 rescue
 end
 
 begin
-    version = content.match(/OS Version:.*?\((.*?)\)\n/m)[1]
-    details << "OS "+version.strip
+  version = content.match(/OS Version:.*?\((.*?)\)\n/m)[1]
+  details << "OS "+version.strip
 rescue
 end
 
--- crashwatcher/related-crash-report.rb
+++ crashwatcher/related-crash-report.rb
@@ -13,28 +13,27 @@ addresses = []
 ranges = []
 
 report.lines.each do |line|
+  # extract adressess from callstacks on all threads
+  # 8   com.apple.AppKit        	0x00007fff88a0d68f -[NSApplication run] + 395
+  if line =~ /^\s*\d+\s+.*?0x([0-9a-f]+)\s/ then
+    addresses << $1.to_i(16)
+  end
 
-    # extract adressess from callstacks on all threads
-    # 8   com.apple.AppKit              	0x00007fff88a0d68f -[NSApplication run] + 395
-    if line =~ /^\s*\d+\s+.*?0x([0-9a-f]+)\s/ then
-        addresses << $1.to_i(16)
-    end
-    
-    # extract loaded module ranges, filter out only com.binaryage related
-    # 0x1181c2000 -        0x1181c3ff7 +com.binaryage.totalfinder.nodesktopdots ##VERSION## (##VERSION##) <1243063C-4405-3DD6-8AC8-816EFA979801> /Applications/TotalFinder.app/Contents/Resources/TotalFinder.bundle/Contents/PlugIns/NoDesktopDots.bundle/Contents/MacOS/NoDesktopDots
-    if line =~ /^\s*0x([0-9a-f]+)\s+-\s+0x([0-9a-f]+)\s+.*?com\.binaryage/ then
-        ranges << [$1.to_i(16), $2.to_i(16)]
-    end
-    
+  # extract loaded module ranges, filter out only com.binaryage related
+  # 0x1181c2000 -    0x1181c3ff7 +com.binaryage.totalfinder.nodesktopdots ##VERSION## (##VERSION##) <1243063C-4405-3DD6-8AC8-816EFA979801> /Applications/TotalFinder.app/Contents/Resources/TotalFinder.bundle/Contents/PlugIns/NoDesktopDots.bundle/Contents/MacOS/NoDesktopDots
+  if line =~ /^\s*0x([0-9a-f]+)\s+-\s+0x([0-9a-f]+)\s+.*?com\.binaryage/ then
+    ranges << [$1.to_i(16), $2.to_i(16)]
+  end
+
 end
 
 # test if any address is present inside our ranges
 addresses.each do |address|
-    ranges.each do |range|
-        hit = address >= range[0] && address <= range[1]
-        exit 1 if hit # yes, present!
-        # puts "#{address} ? #{range[0]} - #{range[1]} => #{hit}"
-    end
+  ranges.each do |range|
+    hit = address >= range[0] && address <= range[1]
+    exit 1 if hit # yes, present!
+    # puts "#{address} ? #{range[0]} - #{range[1]} => #{hit}"
+  end
 end
 
 exit 0 # not present
