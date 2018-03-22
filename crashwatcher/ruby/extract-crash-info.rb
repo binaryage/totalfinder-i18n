0d132eb92bb21faa7d9a694fd1035983415f44b4
Antonin Hildebrand <antonin@hildebrand.cz>
2018-03-22 19:49:35 +0100

    crashwatcher: modernize extract-crash-info.rb

--- crashwatcher/ruby/extract-crash-info.rb
+++ crashwatcher/ruby/extract-crash-info.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 def parse_module_version(content)
   m = content.match(/\+\s*com.binaryage.totalfinder\s*\((.*?)\)/m)
   return nil if m.nil?
   # m[1] == 1.5.18 - 1.5.18
-  m[1].split("-")[0].strip
+  m[1].split('-')[0].strip
 end
 
 def parse_special_info(content)
@@ -25,16 +26,14 @@ def find_first_ba_module(content)
   ranges = []
 
   content.lines.each do |line|
-    # extract adressess from callstacks on all threads
+    # extract addresses from callstacks on all threads
     # 8   com.apple.AppKit        	0x00007fff88a0d68f -[NSApplication run] + 395
-    if line =~ /^\s*\d+\s+.*?0x([0-9a-f]+)\s/ then
-      addresses << $1.to_i(16)
-    end
+    addresses << Regexp.last_match(1).to_i(16) if line =~ /^\s*\d+\s+.*?0x([0-9a-f]+)\s/
 
     # extract loaded module ranges, filter out only com.binaryage related
-    # 0x1181c2000 -    0x1181c3ff7 +com.binaryage.totalfinder.nodesktopdots ##VERSION## (##VERSION##) <1243063C-4405-3DD6-8AC8-816EFA979801> /Applications/TotalFinder.app/Contents/Resources/TotalFinder.bundle/Contents/PlugIns/NoDesktopDots.bundle/Contents/MacOS/NoDesktopDots
-    if line =~ /^\s*0x([0-9a-f]+)\s+-\s+0x([0-9a-f]+)\s+.*?(com\.binaryage.*?)\s/ then
-      ranges << [$1.to_i(16), $2.to_i(16), $3]
+    # 0x1181c2000 -    0x1181c3ff7 +com.binaryage.totalfinder.nodesktopdots ##VERSION## (##VERSION##) <...> /some/path
+    if line.match?(/^\s*0x([0-9a-f]+)\s+-\s+0x([0-9a-f]+)\s+.*?(com\.binaryage.*?)\s/)
+      ranges << [Regexp.last_match(1).to_i(16), Regexp.last_match(2).to_i(16), Regexp.last_match(3)]
     end
   end
 
@@ -50,50 +49,35 @@ end
 file = ARGV[0]
 content = File.read(file)
 
-res = "TotalFinder "
+res = 'TotalFinder '
 
-begin
-  mversion = parse_module_version(content)
-  res += "#{mversion} " if mversion
-  plugin = parse_special_info(content)
-  plugin = parse_plugin_identifier(content) unless plugin
-  plugin = find_first_ba_module(content) unless plugin
-  plugin.strip!
-  res += "crashed in " + plugin.gsub("com.binaryage.totalfinder.", "") + " " if plugin.size
-rescue
-end
+module_version = parse_module_version(content)
+res += "#{module_version} " if module_version
+plugin = parse_special_info(content)
+plugin ||= parse_plugin_identifier(content)
+plugin ||= find_first_ba_module(content)
+plugin.strip!
+res += 'crashed in ' + plugin.gsub('com.binaryage.totalfinder.', '') + ' ' if plugin.size
 
 details = []
 
-begin
-  version = content.match(/Version:(.*?)\n/m)[1].split(" ")[0]
-  details << "v"+version.strip
-rescue
-end
+version = content.match(/Version:(.*?)\n/m)[1].split(' ')[0]
+details << 'v' + version.strip
 
-begin
-  version = content.match(/OS Version:.*?\((.*?)\)\n/m)[1]
-  details << "OS "+version.strip
-rescue
-end
+version = content.match(/OS Version:.*?\((.*?)\)\n/m)[1]
+details << 'OS ' + version.strip
 
-begin
-  type = content.match(/Exception Type:(.*?)\n/m)[1]
-  x = type.match(/(.*?)\(.*\)/)
-  type = x[1] if x # remove braced part if present
-  type.downcase!
-  type.sub!("exc_", "")
-  type.sub!("_", " ")
-  details << type.strip
-rescue
-end
+type = content.match(/Exception Type:(.*?)\n/m)[1]
+x = type.match(/(.*?)\(.*\)/)
+type = x[1] if x # remove braced part if present
+type.downcase!
+type.sub!('exc_', '')
+type.sub!('_', ' ')
+details << type.strip
 
-begin
-  thread = content.match(/Crashed Thread:(.*?)\n/m)[1].split(" ")[0]
-  details << "thread "+thread.strip
-rescue
-end
+thread = content.match(/Crashed Thread:(.*?)\n/m)[1].split(' ')[0]
+details << 'thread ' + thread.strip
 
-res += "| #{details.join(", ")}"
+res += "| #{details.join(', ')}"
 
 puts res
